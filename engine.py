068cdcdfed4967dc285552e43e77d9268dc1b1a4
joshuakwan <garfilone@gmail.com>
2015-11-15 12:42:08 +0800

    create the initial structure

---
+++ engine.py
@@ -0,0 +1,15 @@
+import threading
+
+
+class Engine(threading.Thread):
+    def __init__(self, listener, parser, rule, actions):
+        self._listener = listener
+        self._parser = parser
+        self._rule = rule
+        self._action = actions
+
+
+
+if __name__ == '__main__':
+    engine = Engine()
+    engine.start()
